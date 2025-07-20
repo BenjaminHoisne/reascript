60a4cf4710a6fd8a5de2c068260baa646f7f8c0e
Benjamin Hoisne <156545277+BenjaminHoisne@users.noreply.github.com>
2025-07-20 19:52:57 +0200

    Update CreateRegionFromSelectedItems.lua

--- Utility/CreateRegionFromSelectedItems.lua
+++ Utility/CreateRegionFromSelectedItems.lua
@@ -1,47 +1,47 @@
--- Commencer une action Undo
+-- Start undo action
 reaper.Undo_BeginBlock()
 
--- Table de compteurs pour chaque combinaison Folder+Track
+-- Count for each combination of tracks + folder tracks 
 local counters = {}
 
--- Nombre d'items sélectionnés
+-- Number of selected items
 local item_count = reaper.CountSelectedMediaItems(0)
 
 for i = 0, item_count - 1 do
   local item = reaper.GetSelectedMediaItem(0, i)
   local track = reaper.GetMediaItemTrack(item)
 
-  -- Nom de la piste
+  -- Name of the track
   local _, track_name = reaper.GetTrackName(track)
 
-  -- Nom du dossier parent (folder)
+  -- Name of the folder track
   local folder_track = reaper.GetParentTrack(track)
   local folder_name = "NoFolder"
   if folder_track ~= nil then
     _, folder_name = reaper.GetTrackName(folder_track)
   end
 
-  -- Clé d’identification du groupe
+  -- Group identification key
   local key = folder_name .. "___" .. track_name
 
-  -- Initialiser ou incrémenter le compteur
+  -- Initialize or increment counter
   if counters[key] == nil then
     counters[key] = 1
   else
     counters[key] = counters[key] + 1
   end
 
-  -- Récupérer position et durée de l’item
+  -- Get position and duration of the item
   local pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
   local len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
 
-  -- Créer le nom de la région
+  -- Create region name
   local region_name = string.format("SW_%s_%s_%02d", folder_name, track_name, counters[key])
 
-  -- Créer la région
+  -- Create region
   reaper.AddProjectMarker2(0, true, pos, pos + len, region_name, -1, 0)
 end
 
--- Fin de l’action Undo
+-- End of undo action
 reaper.Undo_EndBlock("Créer régions avec numéros de variation par groupe Folder/Track", -1)
 
